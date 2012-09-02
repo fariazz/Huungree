c9c9f2dd28d939a23b0e070b34abe423b7239732
Pablo Farias Navarro <fariazz@gmail.com>
2012-09-02 20:02:55 -0300

    movement targets in loop

--- player.js
+++ player.js
@@ -93,87 +93,19 @@ huungry.Player.prototype.init = function() {
 
     //register movement according to user input
     player = this;
-    target = this.movementTargets[0];
-    goog.events.listen(this.movementTargets[0].sprite,['mousedown', 'touchstart'], function(e) {
+    
+    for(i=0; i<this.movementTargets.length; i++) {
+        (function (i) {
+            goog.events.listen(player.movementTargets[i].sprite,['mousedown', 'touchstart'], function(e) {
 
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target.dx, currentPos.y + tileSize*target.dy);
-        player.setCanMove(true);
-    });
-
-    target1 = this.movementTargets[1];
-    goog.events.listen(this.movementTargets[1].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target1.dx, currentPos.y + tileSize*target1.dy);
-        player.setCanMove(true);
-    });
-
-    target2 = this.movementTargets[2];
-    goog.events.listen(this.movementTargets[2].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target2.dx, currentPos.y + tileSize*target2.dy);
-        player.setCanMove(true);
-    });
-
-    target3 = this.movementTargets[3];
-    goog.events.listen(this.movementTargets[3].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target3.dx, currentPos.y + tileSize*target3.dy);
-        player.setCanMove(true);
-    });
-
-    target4 = this.movementTargets[4];
-    goog.events.listen(this.movementTargets[4].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target4.dx, currentPos.y + tileSize*target4.dy);
-        player.setCanMove(true);
-    });
-
-    target5 = this.movementTargets[5];
-    goog.events.listen(this.movementTargets[5].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target5.dx, currentPos.y + tileSize*target5.dy);
-        player.setCanMove(true);
-    });
-
-    target6 = this.movementTargets[6];
-    goog.events.listen(this.movementTargets[6].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target6.dx, currentPos.y + tileSize*target6.dy);
-        player.setCanMove(true);
-    });
-
-    target7 = this.movementTargets[7];
-    goog.events.listen(this.movementTargets[7].sprite,['mousedown', 'touchstart'], function(e) {
-
-        e.event.stopPropagation();
-        var currentPos = player.getPosition();
-        var tileSize = player.gameObj.tileSize;
-        player.setPosition(currentPos.x + tileSize*target7.dx, currentPos.y + tileSize*target7.dy);
-        player.setCanMove(true);
-    });
-
-       
+                e.event.stopPropagation();
+                var currentPos = player.getPosition();
+                var tileSize = player.gameObj.tileSize;
+                player.setPosition(currentPos.x + tileSize*player.movementTargets[i].dx, currentPos.y + tileSize*player.movementTargets[i].dy);
+                player.setCanMove(true);
+            });
+        })(i);
+    }       
 }
 
 /*
