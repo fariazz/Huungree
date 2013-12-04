d290c443dbc683576243a635bafef38a6b93fa85
Pablo Farias Navarro <fariazz@gmail.com>
2013-12-04 18:33:48 -0300

    shop fills units evenly

--- shop.js
+++ shop.js
@@ -43,14 +43,32 @@ huungry.Shop.prototype.purchase = function(unit) {
         else {
             //find if there are the same to add up
             var len = this.gameObj.player.units.length;
-            for(var i=0; i < len; i++) {
-                if(this.gameObj.player.units[i].id == unit.id) {
-                    this.gameObj.player.buyAdd(this.gameObj.player.units[i], unit.price, 1);
-                    unit.qty--;
-                    return {success: 1, msg: 'Thanks for your business!'};
+            var sameIndexes = new Array(); 
+            _.each(this.gameObj.player.units, function(value, key){
+                if(value.id == unit.id) {
+                    sameIndexes.push(key);
                 }
+            }, this);
+
+            if(sameIndexes.length) {
+                if(this.prevUnit == unit.id) {
+                    this.nextIndex++;
+                    if(this.nextIndex == sameIndexes.length) {
+                        this.nextIndex = 0;
+                    }
+
+                }
+                else {
+                    this.prevUnit = unit.id;
+                    this.nextIndex = 0;
+                }
+                this.gameObj.player.buyAdd(this.gameObj.player.units[sameIndexes[this.nextIndex]], unit.price, 1);
+                unit.qty--;                    
+                return {success: 1, msg: 'Thanks for your business!'};
             }
-            return {success: 0, msg: 'No room available'};
+            else {
+                return {success: 0, msg: 'No room available'};
+            }            
         }        
     }
     else if(unit.qty == 0) {
@@ -75,83 +93,6 @@ huungry.Shop.prototype.refreshPlayerInfo = function() {
     this.playerResources.setText(this.data.name+' - $'+this.gameObj.player.gold);
 }
 
-/**
- * refresh player's units
- */
-huungry.Shop.prototype.refreshPlayerUnits = function() {
-    this.playerUnitsLayer.removeAllChildren();
-    var gridX = 0, gridY = 0;
-    var playerUnitsRect = new lime.Sprite().setAnchorPoint(0,0).setFill('assets/units_grid.png')
-        .setPosition(gridX,gridY).setSize(this.gameObj.tileSize*5,this.gameObj.tileSize*2);
-    this.playerUnitsLayer.appendChild(playerUnitsRect);    
-    
-    this.thumbnailLayers = new Array();
-    var thumbnail, thumbX, thumbY = gridY+1, lifeBar;
-    for(i=0; i < this.gameObj.player.units.length; i++) {  
-        
-        thumbX = gridX + 1 + i%5*(this.gameObj.tileSize+1);
-        
-        if(i == 5) {
-            thumbY += this.gameObj.tileSize+1;
-        }
-        
-        this.thumbnailLayers.push(
-            new lime.Layer().setAnchorPoint(0,0).setPosition(thumbX, thumbY));
-        
-        thumbnail = new lime.Sprite().setAnchorPoint(0,0)
-            .setSize(this.gameObj.tileSize,this.gameObj.tileSize)
-            .setFill('assets/images/units/'+this.gameObj.player.units[i].image)
-            .setPosition(0,0);        
-        
-        lifeBar = new lime.Label().setPosition(this.gameObj.tileSize/2,this.gameObj.tileSize/2)
-            .setText(Math.ceil(this.gameObj.player.units[i].life)).setFontSize(18)
-            .setAnchorPoint(0,0).setFontColor('#E8FC08');
-        
-        this.thumbnailLayers[i].unit = this.gameObj.player.units[i];
-        this.thumbnailLayers[i].index = i;
-        this.thumbnailLayers[i].appendChild(thumbnail);       
-        this.thumbnailLayers[i].appendChild(lifeBar);       
-        this.playerUnitsLayer.appendChild(this.thumbnailLayers[i]);  
-        
-        (function(thumbnailLayers, i, gameObj, currentObj) {
-            goog.events.listen(thumbnailLayers[i], ['mousedown', 'touchstart'], function(e) {
-                e.stopPropagation();
-                
-                if(thumbnailLayers[i].highlightCell) {
-                    thumbnailLayers[i].removeChild(thumbnailLayers[i].highlightCell);
-                    thumbnailLayers[i].highlightCell = undefined;
-                }
-                else {
-                    //check if a unit is selected, if not, select current
-                    var prevSelected;
-                    for(var j=0, arrLen2 = thumbnailLayers.length; j < arrLen2; j++) {
-                        if(i != j && thumbnailLayers[j].highlightCell) {
-                            prevSelected = thumbnailLayers[j]
-                        }
-                    }
-                    
-                    if(!prevSelected) {                    
-                        thumbnailLayers[i].highlightCell = new lime.Sprite().setAnchorPoint(0,0)
-                            .setPosition(0,0).setSize(gameObj.tileSize, gameObj.tileSize)
-                            .setFill(232,252,8,0.5);
-                        thumbnailLayers[i].appendChild(thumbnailLayers[i].highlightCell);
-                    }
-                    
-                    else {
-                        //if it's the same unit, merge
-                        if(prevSelected.unit.id == thumbnailLayers[i].unit.id) {
-                            gameObj.player.units[i].life += prevSelected.unit.life;
-                            gameObj.player.units.splice(prevSelected.index,1);
-                            thumbnailLayers[prevSelected.index].removeChild(thumbnailLayers[prevSelected.index].highlightCell);
-                            currentObj.refreshPlayerUnits();
-                        }
-                    }
-                }
-                
-            });
-        })(this.thumbnailLayers, i, this.gameObj, this);
-    }
-}
 
 /**
  * get data
